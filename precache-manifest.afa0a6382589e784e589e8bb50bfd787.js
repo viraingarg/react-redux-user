3089339730f76e7f5bd413f79c983b9fa343b0ff
Virain Garg <virain.garg@ad.infosys.com>
2019-06-11 00:19:20 +0530

    Updates

---
+++ precache-manifest.afa0a6382589e784e589e8bb50bfd787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c55b6bd339bb97b3f9d8568404afca9",
+    "url": "/react_redux_user/index.html"
+  },
+  {
+    "revision": "38f15791f7424b16416f",
+    "url": "/react_redux_user/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "2303785efe54cc44bbaa",
+    "url": "/react_redux_user/static/css/main.b8446be5.chunk.css"
+  },
+  {
+    "revision": "38f15791f7424b16416f",
+    "url": "/react_redux_user/static/js/2.7787460c.chunk.js"
+  },
+  {
+    "revision": "2303785efe54cc44bbaa",
+    "url": "/react_redux_user/static/js/main.c88898b4.chunk.js"
+  },
+  {
+    "revision": "bd012e1174b6542a0dbe",
+    "url": "/react_redux_user/static/js/runtime~main.9e6f69fd.js"
+  }
+]);
